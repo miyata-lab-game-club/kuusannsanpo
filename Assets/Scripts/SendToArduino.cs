d2c59371aa794ee6b75cc3756cf2dee87854de69
yamarico <cssu19093@g.nihon-u.ac.jp>
2023-08-14 19:13:58 +0900

    ベクトルじゃなくてコマンド送信

--- Assets/Scripts/SendToArduino.cs
+++ Assets/Scripts/SendToArduino.cs
@@ -20,27 +20,18 @@ public class SendToArduino : MonoBehaviour
         }
     }
 
-    private string ConvertVector3ToString(Vector3 vec, float pullPower)
-    {
-        return string.Format("{0},{1},{2},{3}", vec.x, vec.y, vec.z, pullPower);
-    }
-
     private void Update()
     {
         try
         {
-            Vector3 windData = windManager.windXZDirection[windManager.currentWindIndex];
-            string dataToSend = ConvertVector3ToString(windData, windManager.pullPower);
-
             // Arduinoにデータ送信
             if (serialPort != null && serialPort.IsOpen)
             {
+                // データを":"で区切って送信
+                string dataToSend = $"{windManager.currentWindIndex}:{windManager.pullPower}";
                 serialPort.WriteLine(dataToSend);
+                Debug.Log(dataToSend);
             }
-
-            // For debugging purposes:
-            // Debug.Log(windManager.pullPower);
-            Debug.Log(dataToSend);
         }
         catch (Exception ex)
         {
