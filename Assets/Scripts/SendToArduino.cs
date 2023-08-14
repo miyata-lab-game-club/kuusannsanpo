269cb461f6503b821fc332fb9240f79df11a2a4f
yamarico <cssu19093@g.nihon-u.ac.jp>
2023-08-14 23:26:33 +0900

    upをint型にして3つの値を送信

--- Assets/Scripts/SendToArduino.cs
+++ Assets/Scripts/SendToArduino.cs
@@ -27,8 +27,10 @@ public class SendToArduino : MonoBehaviour
             // Arduinoにデータ送信
             if (serialPort != null && serialPort.IsOpen)
             {
+                int upValue = windManager.up ? 1 : 0;  // この行でboolをintに変換するTrueが1でFalseが0
+
                 // データを":"で区切って送信
-                string dataToSend = $"{windManager.currentWindIndex}:{windManager.pullPower}";
+                string dataToSend = $"{windManager.currentWindIndex}:{windManager.pullPower}:{upValue}";
                 serialPort.WriteLine(dataToSend);
                 Debug.Log(dataToSend);
             }
