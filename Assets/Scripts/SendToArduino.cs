bf969aa2acdb42f27730979984a2797904b473b6
yamarico <cssu19093@g.nihon-u.ac.jp>
2023-08-15 03:25:43 +0900

    フラグを置いた

--- Assets/Scripts/SendToArduino.cs
+++ Assets/Scripts/SendToArduino.cs
@@ -20,7 +20,7 @@ public class SendToArduino : MonoBehaviour
         }
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         try
         {
@@ -30,7 +30,7 @@ public class SendToArduino : MonoBehaviour
                 int upValue = windManager.up ? 1 : 0;  // この行でboolをintに変換するTrueが1でFalseが0
 
                 // データを":"で区切って送信
-                string dataToSend = $"{windManager.currentWindIndex}:{windManager.pullPower}:{upValue}";
+                string dataToSend = $"{windManager.currentWindIndex}:{windManager.pullPower}:{upValue}:{"a"}";
                 serialPort.WriteLine(dataToSend);
                 Debug.Log(dataToSend);
             }
